eaf8b4ad82bb7c29c8a574954aa4f12c93d7dd73
d1slike <merser1q@gmail.com>
2021-12-14 14:15:18 +0300

    MSP-411 implement client middlewares

---
+++ grpc/client/middlewares.go
@@ -0,0 +1,36 @@
+package client
+
+import (
+	"context"
+	"time"
+
+	"github.com/integration-system/isp-kit/grpc"
+	"github.com/integration-system/isp-kit/grpc/isp"
+	"github.com/integration-system/isp-kit/requestid"
+	"google.golang.org/grpc/metadata"
+)
+
+func RequestId() Middleware {
+	return func(next RoundTripper) RoundTripper {
+		return func(ctx context.Context, message *isp.Message) (*isp.Message, error) {
+			requestId := requestid.FromContext(ctx)
+			if requestId == "" {
+				requestId = requestid.Next()
+			}
+
+			ctx = metadata.AppendToOutgoingContext(ctx, grpc.RequestIdHeader, requestId)
+			return next(ctx, message)
+		}
+	}
+}
+
+func DefaultTimeout(timeout time.Duration) Middleware {
+	return func(next RoundTripper) RoundTripper {
+		return func(ctx context.Context, message *isp.Message) (*isp.Message, error) {
+			ctx, cancel := context.WithTimeout(ctx, timeout)
+			defer cancel()
+
+			return next(ctx, message)
+		}
+	}
+}
