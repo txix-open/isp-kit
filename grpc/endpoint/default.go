33754ef91dfce8b251d5d633cc4ff9c2402e1fde
d1slike <merser1q@gmail.com>
2022-02-01 18:11:14 +0300

    Add rest middlewares vararg param

--- grpc/endpoint/default.go
+++ grpc/endpoint/default.go
@@ -5,18 +5,22 @@ import (
 	"github.com/integration-system/isp-kit/validator"
 )
 
-func DefaultWrapper(logger log.Logger) Wrapper {
+func DefaultWrapper(logger log.Logger, restMiddlewares ...Middleware) Wrapper {
 	paramMappers := []ParamMapper{
 		ContextParam(),
 		AuthDataParam(),
 	}
+	middlewares := append(
+		[]Middleware{
+			RequestId(),
+			ErrorHandler(logger),
+			Recovery(),
+		},
+		restMiddlewares...,
+	)
 	return NewWrapper(
 		paramMappers,
 		JsonRequestExtractor{validator: validator.Default},
 		JsonResponseMapper{},
-	).WithMiddlewares(
-		RequestId(),
-		ErrorHandler(logger),
-		Recovery(),
-	)
+	).WithMiddlewares(middlewares...)
 }
