19fecfef234f2a67385591e06d15d7a0ea293ef3
d1slike <merser1q@gmail.com>
2022-04-04 19:19:19 +0300

    MSP-409 add basic http benchmarks

---
+++ http/bench/go.mod
@@ -0,0 +1,42 @@
+module github.com/integration-system/isp-kit/http/bench
+
+go 1.18
+
+require (
+	github.com/gin-gonic/gin v1.7.7
+	github.com/google/uuid v1.3.0
+	github.com/integration-system/isp-kit v1.3.0
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/labstack/echo/v4 v4.7.2
+)
+
+require (
+	github.com/BurntSushi/toml v1.0.0 // indirect
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.10.1 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/ugorji/go/codec v1.2.7 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
+	go.uber.org/zap v1.20.0 // indirect
+	golang.org/x/crypto v0.0.0-20220331220935-ae2d96664a29 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/sys v0.0.0-20220403020550-483a9cbc67c0 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ http/bench/http.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"context"
+	"io"
+	"net"
+	stdHttp "net/http"
+	"os"
+	"os/signal"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/integration-system/isp-kit/http"
+	"github.com/integration-system/isp-kit/http/endpoint"
+	"github.com/integration-system/isp-kit/log"
+	"github.com/integration-system/isp-kit/validator"
+	"github.com/julienschmidt/httprouter"
+	"github.com/labstack/echo/v4"
+)
+
+type Request struct {
+	S string `valid:"required"`
+}
+
+type Response struct {
+	FromReq string
+	Resp    string
+}
+
+func main() {
+	ServeIspHttp()
+	ServeEchoHttp()
+	ServeGinHttp()
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+	<-ch
+}
+
+func ServeIspHttp() {
+	logger, err := log.New()
+	if err != nil {
+		panic(err)
+	}
+	listener, err := net.Listen("tcp", "127.0.0.1:8000")
+	if err != nil {
+		panic(err)
+	}
+	s := http.NewHttpServer()
+	go func() {
+		err := s.Serve(listener)
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	handler := func(ctx context.Context, req Request) (*Response, error) {
+		return &Response{
+			FromReq: req.S,
+			Resp:    uuid.New().String(),
+		}, nil
+	}
+	httpHandler := endpoint.DefaultWrapper(logger).Endpoint(handler)
+	mux := httprouter.New()
+	mux.Handler(stdHttp.MethodPost, "/post", httpHandler)
+	s.Upgrade(mux)
+}
+
+type v struct {
+}
+
+func (v v) Validate(i interface{}) error {
+	return validator.Default.ValidateToError(i)
+}
+
+func ServeEchoHttp() {
+	listener, err := net.Listen("tcp", "127.0.0.1:8001")
+	if err != nil {
+		panic(err)
+	}
+	e := echo.New()
+	e.Validator = v{}
+	e.Listener = listener
+	e.HideBanner = true
+	e.Logger.SetOutput(io.Discard)
+
+	e.POST("/post", func(c echo.Context) error {
+		req := Request{}
+		err := c.Bind(&req)
+		if err != nil {
+			return err
+		}
+		err = c.Validate(req)
+		if err != nil {
+			return err
+		}
+		err = c.JSON(stdHttp.StatusOK, Response{
+			FromReq: req.S,
+			Resp:    uuid.NewString(),
+		})
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	go func() {
+		err := e.Start("")
+		if err != nil {
+			panic(err)
+		}
+	}()
+}
+
+func ServeGinHttp() {
+	listener, err := net.Listen("tcp", "127.0.0.1:8002")
+	if err != nil {
+		panic(err)
+	}
+	gin.SetMode(gin.ReleaseMode)
+	g := gin.New()
+	g.POST("/post", func(c *gin.Context) {
+		req := Request{}
+		err := c.BindJSON(&req)
+		if err != nil {
+			_ = c.AbortWithError(stdHttp.StatusBadRequest, err)
+		}
+		err = validator.Default.ValidateToError(req)
+		if err != nil {
+			_ = c.AbortWithError(stdHttp.StatusBadRequest, err)
+		}
+		c.JSON(stdHttp.StatusOK, Response{
+			FromReq: req.S,
+			Resp:    uuid.NewString(),
+		})
+	})
+
+	go func() {
+		err := g.RunListener(listener)
+		if err != nil {
+			panic(err)
+		}
+	}()
+}
