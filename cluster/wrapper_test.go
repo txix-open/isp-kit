89016522aefdcdde0e9bb8689efb15f7a0ad2f37
nergilz <nergilz@yandex.ru>
2022-05-18 13:02:15 +0200

    MSP-659 test hide secrets

---
+++ cluster/wrapper_test.go
@@ -0,0 +1,86 @@
+package cluster_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/integration-system/isp-kit/cluster"
+	"github.com/stretchr/testify/require"
+)
+
+func TestConfig_Log_Replace(t *testing.T) {
+	require := require.New(t)
+	configData := []byte(ConfigData)
+	expectData := []byte(ExpectData)
+
+	actualData, err := cluster.HideSecrets(configData)
+	require.NoError(err)
+
+	expectConfig := make(map[string]interface{})
+	actualConfig := make(map[string]interface{})
+
+	err = json.Unmarshal(expectData, &expectConfig)
+	require.NoError(err)
+	err = json.Unmarshal(actualData, &actualConfig)
+	require.NoError(err)
+
+	require.Equal(expectConfig, actualConfig)
+}
+
+const ExpectData = `
+{
+	"secret": {
+		"1": "***",
+		"2": "***"
+	},
+	"db": {
+		"database": "dbname",
+		"password": "***",
+		"username": "dbname",
+		"host": "101.00.11.11",
+		"port": 5433
+	},
+	"rabbit": {
+		"client": {
+			"password": "***",
+			"username": "username",
+			"host": "102.00.22.22",
+			"port": 5673
+		}
+	},
+	"Test": {
+		"time": "80h",
+		"Secret": ""
+	},
+	"token":"***" 
+}
+`
+
+const ConfigData = `
+{
+	"secret": {
+		"1": "***",
+		"2": "***"
+	},
+	"db": {
+		"database": "dbname",
+		"password": "***",
+		"username": "dbname",
+		"host": "101.00.11.11",
+		"port": 5433
+	},
+	"rabbit": {
+		"client": {
+			"password": "***",
+			"username": "username",
+			"host": "102.00.22.22",
+			"port": 5673
+		}
+	},
+	"Test": {
+		"time": "80h",
+		"Secret": ""
+	},
+	"token":"***" 
+}
+`
