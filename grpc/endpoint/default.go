ca6c4b3e591c33676ce98d38b11874f5754ecd5e
d1slike <merser1q@gmail.com>
2022-07-14 16:13:22 +0300

    MSP-822 Add grpc metrics middleware by default

--- grpc/endpoint/default.go
+++ grpc/endpoint/default.go
@@ -2,6 +2,8 @@ package endpoint
 
 import (
 	"github.com/integration-system/isp-kit/log"
+	"github.com/integration-system/isp-kit/metrics"
+	"github.com/integration-system/isp-kit/metrics/grpc_metrics"
 	"github.com/integration-system/isp-kit/validator"
 )
 
@@ -10,8 +12,10 @@ func DefaultWrapper(logger log.Logger, restMiddlewares ...Middleware) Wrapper {
 		ContextParam(),
 		AuthDataParam(),
 	}
+	metricStorage := grpc_metrics.NewStorage(metrics.DefaultRegistry)
 	middlewares := append(
 		[]Middleware{
+			Metrics(metricStorage),
 			RequestId(),
 			ErrorHandler(logger),
 			Recovery(),
