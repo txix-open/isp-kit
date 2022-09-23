6732c1913d42414dcf2e4e5eecf60953d6729c7a
d1slike <merser1q@gmail.com>
2022-09-23 15:29:01 +0300

    MSP-1024 implement metric and log middleware

--- grpc/client/middlewares.go
+++ grpc/client/middlewares.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/integration-system/isp-kit/grpc"
 	"github.com/integration-system/isp-kit/grpc/isp"
+	"github.com/integration-system/isp-kit/log"
 	"github.com/integration-system/isp-kit/requestid"
 	"google.golang.org/grpc/metadata"
 )
@@ -19,7 +20,7 @@ func RequestId() Middleware {
 			}
 
 			ctx = metadata.AppendToOutgoingContext(ctx, grpc.RequestIdHeader, requestId)
-			return next(ctx, message)
+			return next(ctx, builder, message)
 		}
 	}
 }
@@ -30,7 +31,49 @@ func DefaultTimeout(timeout time.Duration) Middleware {
 			ctx, cancel := context.WithTimeout(ctx, timeout)
 			defer cancel()
 
-			return next(ctx, message)
+			return next(ctx, builder, message)
+		}
+	}
+}
+
+func Log(logger log.Logger) Middleware {
+	return func(next RoundTripper) RoundTripper {
+		return func(ctx context.Context, builder *RequestBuilder, message *isp.Message) (*isp.Message, error) {
+			logger.Debug(
+				ctx,
+				"grpc client: request",
+				log.String("requestEndpoint", builder.endpoint),
+				log.ByteString("requestBody", message.GetBytesBody()),
+			)
+
+			resp, err := next(ctx, builder, message)
+			if err != nil {
+				logger.Debug(ctx, "grpc client: response", log.Any("error", err))
+				return resp, err
+			}
+
+			logger.Debug(
+				ctx,
+				"grpc client: response",
+				log.ByteString("responseBody", resp.GetBytesBody()),
+			)
+
+			return resp, err
+		}
+	}
+}
+
+type MetricStorage interface {
+	ObserveDuration(endpoint string, duration time.Duration)
+}
+
+func Metrics(storage MetricStorage) Middleware {
+	return func(next RoundTripper) RoundTripper {
+		return func(ctx context.Context, builder *RequestBuilder, message *isp.Message) (*isp.Message, error) {
+			start := time.Now()
+			response, err := next(ctx, builder, message)
+			storage.ObserveDuration(builder.endpoint, time.Since(start))
+			return response, err
 		}
 	}
 }
