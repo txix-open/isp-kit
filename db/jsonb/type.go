010f443123e486590987e53b0d672269b2199579
novikovmu <novikovmu@elocont.ru>
2022-10-17 11:34:32 +0300

    MSP-1110 change jsonb type

--- db/jsonb/type.go
+++ db/jsonb/type.go
@@ -1,29 +1,3 @@
 package jsonb
 
-import (
-	"database/sql/driver"
-
-	"github.com/jackc/pgtype"
-)
-
-type Type []byte
-
-func (t *Type) Scan(src interface{}) error {
-	jsonb := pgtype.JSONB{}
-	err := jsonb.Scan(src)
-	if err != nil {
-		return err
-	}
-	if jsonb.Status == pgtype.Null {
-		return nil
-	}
-	*t = jsonb.Bytes
-	return nil
-}
-
-func (t Type) Value() (driver.Value, error) {
-	if t == nil {
-		return pgtype.JSONB{Status: pgtype.Null}.Value()
-	}
-	return pgtype.JSONB{Status: pgtype.Present, Bytes: t}.Value()
-}
+type Type = []byte
