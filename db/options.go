afddd10297e60b9cf4113b141ce9f7c8b11d3987
novikovmu <novikovmu@elocont.ru>
2022-12-02 12:19:19 +0300

    MSP-1213 add tracer to db pkg

--- db/options.go
+++ db/options.go
@@ -1,3 +1,13 @@
 package db
 
+import (
+	"github.com/jackc/pgx/v5"
+)
+
 type Option func(db *Client)
+
+func WithTracer(tracer pgx.QueryTracer) Option {
+	return func(db *Client) {
+		db.t = tracer
+	}
+}
