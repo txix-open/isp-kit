405f1ea1292d1896c5cc32aafa831d95a7a70925
d1slike <merser1q@gmail.com>
2023-09-15 14:19:59 +0300

    Fix pprof routes

--- infra/pprof/init.go
+++ infra/pprof/init.go
@@ -6,16 +6,20 @@ import (
 	"net/http/pprof"
 )
 
+//go tool pprof -http=0.0.0.0:6061 http://localhost:10000/internal/debug/pprof/allocs
+//go tool pprof -http=0.0.0.0:6061 http://localhost:10000/internal/debug/pprof/profile?seconds=10
+//go tool pprof http://localhost:10000/internal/debug/pprof/profile?seconds=10 > profile.out
+
 type Muxer interface {
-	HandleFunc(patter string, handler http.HandlerFunc)
+	Handle(patter string, handler http.Handler)
 }
 
 func RegisterHandlers(prefix string, muxer Muxer) {
-	muxer.HandleFunc(fmt.Sprintf("%s/debug/pprof/", prefix), pprof.Index)
-	muxer.HandleFunc(fmt.Sprintf("%s/debug/pprof/cmdline", prefix), pprof.Cmdline)
-	muxer.HandleFunc(fmt.Sprintf("%s/debug/pprof/profile", prefix), pprof.Profile)
-	muxer.HandleFunc(fmt.Sprintf("%s/debug/pprof/symbol", prefix), pprof.Symbol)
-	muxer.HandleFunc(fmt.Sprintf("%s/debug/pprof/trace", prefix), pprof.Trace)
+	muxer.Handle(fmt.Sprintf("%s/debug/pprof/", prefix), cutPrefix(prefix, pprof.Index))
+	muxer.Handle(fmt.Sprintf("%s/debug/pprof/cmdline", prefix), cutPrefix(prefix, pprof.Cmdline))
+	muxer.Handle(fmt.Sprintf("%s/debug/pprof/profile", prefix), cutPrefix(prefix, pprof.Profile))
+	muxer.Handle(fmt.Sprintf("%s/debug/pprof/symbol", prefix), cutPrefix(prefix, pprof.Symbol))
+	muxer.Handle(fmt.Sprintf("%s/debug/pprof/trace", prefix), cutPrefix(prefix, pprof.Trace))
 }
 
 func Endpoints(prefix string) []string {
@@ -26,4 +30,11 @@ func Endpoints(prefix string) []string {
 		fmt.Sprintf("%s/debug/pprof/symbol", prefix),
 		fmt.Sprintf("%s/debug/pprof/trace", prefix),
 	}
+}
+
+func cutPrefix(prefix string, handler http.HandlerFunc) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		request.URL.Path = request.URL.Path[len(prefix):]
+		handler.ServeHTTP(writer, request)
+	})
 }
