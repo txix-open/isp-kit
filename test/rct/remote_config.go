2df94c7c447b6a978f8cce9b4671e3db5bcccf04
Kirill Ryabov <ryabovka@txix.ru>
2023-12-05 18:59:56 +0700

    MSP-2460 Add check for old validator tag in structs

--- test/rct/remote_config.go
+++ test/rct/remote_config.go
@@ -1,7 +1,9 @@
 package rct
 
 import (
+	"fmt"
 	"os"
+	"reflect"
 	"testing"
 
 	"github.com/integration-system/isp-kit/json"
@@ -13,6 +15,10 @@ import (
 
 func Test[T any](t *testing.T, defaultRemoteConfigPath string, remoteConfig T) {
 	require := require.New(t)
+
+	validatorTag := "valid"
+	require.False(FindTag(remoteConfig, validatorTag),
+		fmt.Sprintf("'%s' tag is is not longer supported, you must use https://github.com/go-playground/validator", validatorTag))
 
 	defaultRemoteConfig, err := os.ReadFile(defaultRemoteConfigPath)
 	require.NoError(err)
@@ -38,4 +44,32 @@ func Test[T any](t *testing.T, defaultRemoteConfigPath string, remoteConfig T) {
 	require.NoError(err)
 	err = validator.Default.ValidateToError(remoteConfig)
 	require.NoError(err)
+}
+
+func FindTag[T any](v T, tag string) bool {
+	t := reflect.TypeOf(v)
+	if t == nil {
+		return false
+	}
+	queue := []reflect.Type{t}
+	for len(queue) > 0 {
+		cur := queue[0]
+		queue = queue[1:]
+		if cur.Kind() == reflect.Ptr {
+			cur = cur.Elem()
+		}
+		switch cur.Kind() {
+		case reflect.Struct:
+			for i := 0; i < cur.NumField(); i++ {
+				field := cur.Field(i)
+				if field.Tag.Get(tag) != "" {
+					return true
+				}
+				queue = append(queue, field.Type)
+			}
+		case reflect.Map, reflect.Array, reflect.Slice:
+			queue = append(queue, cur.Elem())
+		}
+	}
+	return false
 }
--- test/rct/remote_config_test.go
+++ test/rct/remote_config_test.go
@@ -4,18 +4,35 @@ import (
 	"testing"
 
 	"github.com/integration-system/isp-kit/test/rct"
+	"github.com/stretchr/testify/assert"
 )
 
 type Child struct {
-	Value string `valid:"required"`
+	Value string `validate:"required"`
 }
 
 type Config struct {
-	String  string `valid:"required"`
+	String  string `validate:"required"`
 	Integer int
 	Child   Child
 }
 
 func Test(t *testing.T) {
 	rct.Test(t, "config.json", Config{})
+}
+
+func TestFindTag(t *testing.T) {
+	tag := "validate"
+	assert.True(t, rct.FindTag(Config{}, tag))
+	assert.True(t, rct.FindTag(&Config{}, tag))
+	assert.True(t, rct.FindTag[*Config](nil, tag))
+	type s struct {
+		Cfg Config
+	}
+	assert.True(t, rct.FindTag(s{}, tag))
+	assert.True(t, rct.FindTag(map[string]Config{}, tag))
+	assert.True(t, rct.FindTag(map[string]*Config{}, tag))
+	assert.True(t, rct.FindTag([]*Config{}, tag))
+	assert.True(t, rct.FindTag([]Config{}, tag))
+	assert.True(t, rct.FindTag[[]map[string][]*s](nil, tag))
 }
