fba19baaefcda2801330002633b7e99dc833d0f5
yankomissarov <komissarovyv@txix.ru>
2024-10-28 13:45:49 +0300

    Add request level middlewares

---
+++ http/httpcli/middlewares.go
@@ -0,0 +1,20 @@
+package httpcli
+
+import (
+	"context"
+)
+
+func SetContentLength() Middleware {
+	transferEncoding := []string{"identity"}
+	return func(next RoundTripper) RoundTripper {
+		return RoundTripperFunc(func(ctx context.Context, request *Request) (*Response, error) {
+			if request.body == nil { //multipart
+				return next.RoundTrip(ctx, request)
+			}
+
+			request.Raw.ContentLength = int64(len(request.body))
+			request.Raw.TransferEncoding = transferEncoding
+			return next.RoundTrip(ctx, request)
+		})
+	}
+}
