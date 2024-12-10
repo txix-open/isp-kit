368726a50d1d1d2e9dd57b4b9b1f3f7c78aa5797
yankomissarov <komissarovyv@txix.ru>
2024-12-10 19:00:44 +0300

    Reimplement kafkax

--- kafkax/client.go
+++ kafkax/client.go
@@ -12,10 +12,22 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
+type state struct {
+	publishers []*publisher.Publisher
+	consumers  []consumer.Consumer
+	observer   Observer
+}
+
+func (c *state) run(ctx context.Context) {
+	for _, consumer := range c.consumers {
+		consumer.Run(ctx)
+	}
+}
+
 type Client struct {
 	logger  log.Logger
 	prevCfg Config
-	cli     *KafkaClient
+	state   *state
 	lock    sync.Locker
 }
 
@@ -23,30 +35,46 @@ func New(logger log.Logger) *Client {
 	return &Client{
 		logger:  logger,
 		prevCfg: Config{},
-		cli:     nil,
+		state:   nil,
 		lock:    &sync.Mutex{},
 	}
 }
 
-type KafkaClient struct {
-	publishers []*publisher.Publisher
-	consumers  []consumer.Consumer
-	observer   Observer
-}
+func (c *Client) UpgradeAndServe(ctx context.Context, config Config) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
 
-func (c *Client) UpgradeAndServe(ctx context.Context, config Config) error {
-	return c.upgradeAndServe(ctx, config)
+	c.logger.Debug(ctx, "kafka client: received new config")
+
+	if reflect.DeepEqual(c.prevCfg, config) {
+		c.logger.Debug(ctx, "kafka client: configs are equal. skipping initialization")
+		return
+	}
+
+	c.logger.Debug(ctx, "kafka client: initialization began")
+
+	if c.state != nil {
+		c.Shutdown()
+		c.state = nil
+	}
+
+	c.state = c.newState(ctx, config)
+	c.state.run(ctx)
+
+	c.state.observer.ClientReady()
+
+	c.prevCfg = config
 }
 
 func (c *Client) Healthcheck(ctx context.Context) error {
-	for _, consumer := range c.cli.consumers {
+	for _, consumer := range c.state.consumers {
 		err := consumer.Healthcheck(ctx)
 		if err != nil {
 			return errors.WithMessage(err, "kafka consumer healthcheck")
 		}
 	}
 
-	for _, publisher := range c.cli.publishers {
+	for _, publisher := range c.state.publishers {
 		err := publisher.Healthcheck(ctx)
 		if err != nil {
 			return errors.WithMessage(err, "kafka publisher healthcheck")
@@ -60,78 +88,45 @@ func (c *Client) Close() {
 	c.lock.Lock()
 	defer c.lock.Unlock()
 
-	if c.cli == nil {
+	if c.state == nil {
 		return
 	}
 	c.Shutdown()
-	c.cli = nil
+	c.state = nil
 }
 
 func (c *Client) Shutdown() {
-	c.cli.observer.ShutdownStarted()
+	c.state.observer.ShutdownStarted()
 
 	closeGroup, _ := errgroup.WithContext(context.Background())
-	for _, consumer := range c.cli.consumers {
+	for _, consumer := range c.state.consumers {
 		closeGroup.Go(func() error {
 			err := consumer.Close()
 			if err != nil {
-				c.cli.observer.ClientError(err)
+				c.state.observer.ClientError(err)
 			}
 			return nil
 		})
 	}
 
-	for _, publisher := range c.cli.publishers {
+	for _, publisher := range c.state.publishers {
 		closeGroup.Go(func() error {
 			err := publisher.Close()
 			if err != nil {
-				c.cli.observer.ClientError(err)
+				c.state.observer.ClientError(err)
 			}
 			return nil
 		})
 	}
 
 	_ = closeGroup.Wait()
-	c.cli.observer.ShutdownDone()
+	c.state.observer.ShutdownDone()
 }
 
-func (c *Client) upgradeAndServe(ctx context.Context, config Config) error {
-	c.lock.Lock()
-	defer c.lock.Unlock()
-
-	c.logger.Debug(ctx, "kafka client: received new config")
-
-	if reflect.DeepEqual(c.prevCfg, config) {
-		c.logger.Debug(ctx, "kafka client: configs are equal. skipping initialization")
-		return nil
-	}
-
-	c.logger.Debug(ctx, "kafka client: initialization began")
-
-	if c.cli != nil {
-		c.Shutdown()
-		c.cli = nil
-	}
-
-	c.cli = c.upgrade(ctx, config)
-	c.cli.run(ctx)
-
-	c.cli.observer.ClientReady()
-
-	c.prevCfg = config
-	return nil
-}
-
-func (c *Client) upgrade(ctx context.Context, config Config) *KafkaClient {
-	return &KafkaClient{
+func (c *Client) newState(ctx context.Context, config Config) *state {
+	return &state{
 		publishers: config.Publishers,
 		consumers:  config.Consumers,
 		observer:   NewLogObserver(ctx, c.logger),
-	}
-}
-
-func (c *KafkaClient) run(ctx context.Context) {
-	for _, consumer := range c.consumers {
-		consumer.Run(ctx)
 	}
 }
