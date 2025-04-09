82964549ae3ff09b5b302ccdd328b876f027815c
txix <txix@txix.ru>
2025-04-09 15:43:13 +0300

    2025-04-09 15:43:12: merge [main:e0f4109e0a74876fe2ccc2b7653e30b99ac00441];

--- cluster/wrapper.go
+++ cluster/wrapper.go
@@ -2,19 +2,32 @@ package cluster
 
 import (
 	"context"
-	"github.com/txix-open/etp/v4"
+	"sync"
 	"time"
+
+	"github.com/txix-open/etp/v4"
 
 	"github.com/pkg/errors"
 	"github.com/txix-open/etp/v4/msg"
+	"github.com/txix-open/isp-kit/json"
 	"github.com/txix-open/isp-kit/log"
 )
 
+const (
+	emitWithAckTimeout = 1 * time.Second
+)
+
+type eventFuture struct {
+	responseCh chan []byte
+	errorCh    chan error
+}
+
 type clientWrapper struct {
 	cli             *etp.Client
-	errorChan       chan []byte
+	errorConnChan   chan []byte
 	configErrorChan chan []byte
 	ctx             context.Context
+	eventFutures    sync.Map // map[eventName]eventFuture
 	logger          log.Logger
 }
 
@@ -24,10 +37,8 @@ func newClientWrapper(ctx context.Context, cli *etp.Client, logger log.Logger) *
 		ctx:    ctx,
 		logger: logger,
 	}
-	errorChan := w.EventChan(ErrorConnection)
-	configErrorChan := w.EventChan(ConfigError)
-	w.errorChan = errorChan
-	w.configErrorChan = configErrorChan
+	w.errorConnChan = w.eventChan(ErrorConnection)
+	w.configErrorChan = w.eventChan(ConfigError)
 	cli.OnUnknownEvent(etp.HandlerFunc(func(ctx context.Context, conn *etp.Conn, event msg.Event) []byte {
 		logger.Error(
 			ctx,
@@ -40,17 +51,17 @@ func newClientWrapper(ctx context.Context, cli *etp.Client, logger log.Logger) *
 	return w
 }
 
-func (w *clientWrapper) On(event string, handler func(data []byte)) {
-	w.cli.On(event, etp.HandlerFunc(func(ctx context.Context, conn *etp.Conn, event msg.Event) []byte {
-		w.logger.Info(
-			w.ctx,
-			"event received",
-			log.String("event", event.Name),
-			log.ByteString("data", hideSecrets(event.Name, event.Data)),
-		)
-		handler(event.Data)
-		return nil
-	}))
+func (w *clientWrapper) EmitJsonWithAck(ctx context.Context, event string, data any) ([]byte, error) {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return nil, errors.WithMessagef(err, "marshal '%s' data", event)
+	}
+
+	resp, err := w.EmitWithAck(ctx, event, jsonData)
+	if err != nil {
+		return nil, errors.WithMessage(err, "emit with ask")
+	}
+	return resp, nil
 }
 
 func (w *clientWrapper) EmitWithAck(ctx context.Context, event string, data []byte) ([]byte, error) {
@@ -61,7 +72,7 @@ func (w *clientWrapper) EmitWithAck(ctx context.Context, event string, data []by
 		log.ByteString("data", hideSecrets(event, data)),
 	)
 
-	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	ctx, cancel := context.WithTimeout(ctx, emitWithAckTimeout)
 	defer cancel()
 
 	resp, err := w.cli.EmitWithAck(ctx, event, data)
@@ -74,30 +85,48 @@ func (w *clientWrapper) EmitWithAck(ctx context.Context, event string, data []by
 	return resp, err
 }
 
-func (w *clientWrapper) EventChan(event string) chan []byte {
-	ch := make(chan []byte, 1)
-	w.On(event, func(data []byte) {
-		select {
-		case <-w.ctx.Done():
-		case ch <- data:
+func (w *clientWrapper) RegisterEvent(event string, handler func([]byte) error) {
+	future := eventFuture{
+		errorCh:    make(chan error, 1),
+		responseCh: make(chan []byte, 1),
+	}
+	w.eventFutures.Store(event, future)
+
+	w.on(event, func(data []byte) {
+		err := handler(data)
+		if err != nil {
+			sendNonBlocking(future.errorCh, err)
+			w.logger.Error(w.ctx, "handle event",
+				log.String("event", event),
+				log.String("error", err.Error()),
+			)
+			return
 		}
+		sendNonBlocking(future.responseCh, data)
 	})
-	return ch
 }
 
-func (w *clientWrapper) Await(ctx context.Context, ch chan []byte, timeout time.Duration) ([]byte, error) {
+func (w *clientWrapper) AwaitEvent(ctx context.Context, event string, timeout time.Duration) error {
 	ctx, cancel := context.WithTimeout(ctx, timeout)
 	defer cancel()
 
+	futureVal, ok := w.eventFutures.Load(event)
+	if !ok {
+		return errors.Errorf("event %s not registered", event)
+	}
+	future := futureVal.(eventFuture)
+
 	select {
 	case <-ctx.Done():
-		return nil, ctx.Err()
-	case data := <-w.errorChan:
-		return nil, errors.New(string(data))
+		return ctx.Err()
+	case data := <-w.errorConnChan:
+		return errors.New(string(data))
 	case data := <-w.configErrorChan:
-		return nil, errors.New(string(data))
-	case data := <-ch:
-		return data, nil
+		return errors.New(string(data))
+	case err := <-future.errorCh:
+		return err
+	case <-future.responseCh:
+		return nil
 	}
 }
 
@@ -111,4 +140,39 @@ func (w *clientWrapper) Close() error {
 
 func (w *clientWrapper) Dial(ctx context.Context, url string) error {
 	return w.cli.Dial(ctx, url)
+}
+
+func (w *clientWrapper) OnDisconnect(handler etp.DisconnectHandler) {
+	w.cli.OnDisconnect(handler)
+}
+
+func (w *clientWrapper) eventChan(event string) chan []byte {
+	ch := make(chan []byte, 1)
+	w.on(event, func(data []byte) {
+		select {
+		case <-w.ctx.Done():
+		case ch <- data:
+		}
+	})
+	return ch
+}
+
+func (w *clientWrapper) on(event string, handler func(data []byte)) {
+	w.cli.On(event, etp.HandlerFunc(func(ctx context.Context, conn *etp.Conn, event msg.Event) []byte {
+		w.logger.Info(
+			w.ctx,
+			"event received",
+			log.String("event", event.Name),
+			log.ByteString("data", hideSecrets(event.Name, event.Data)),
+		)
+		handler(event.Data)
+		return nil
+	}))
+}
+
+func sendNonBlocking[T any](ch chan T, data T) {
+	select {
+	case ch <- data:
+	default:
+	}
 }
