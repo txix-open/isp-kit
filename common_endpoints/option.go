3600021d445e8251ee1eb3eb6a0f147a7603d20a
yankomissarov <komissarovyv@txix.ru>
2026-04-16 20:41:18 +0300

    Add doc for common endpoints package

--- common_endpoints/option.go
+++ common_endpoints/option.go
@@ -1,7 +1,13 @@
+// Package common_endpoints provides common HTTP endpoint descriptors for services.
+// It offers a simple way to register standard endpoints like Swagger documentation.
 package common_endpoints
 
+// CommonEndpointOption is a functional option for configuring commonEndpointsCfg.
 type CommonEndpointOption func(cfg *commonEndpointsCfg)
 
+// WithSwaggerEndpoint configures the CommonEndpoints builder to include a Swagger endpoint.
+// The swagger parameter should contain the JSON-formatted Swagger specification.
+// The endpoint is exposed at {basePath}/swagger and is accessible via HTTP GET.
 func WithSwaggerEndpoint(swagger []byte) CommonEndpointOption {
 	return func(cfg *commonEndpointsCfg) {
 		cfg.swagger = swagger
