d57acab065689bccdb6044c8067071e1b98c19b0
Синельник Тимур Романович <sinelnik.timur@yandex.ru>
2026-05-29 15:56:17 +0300

    MSP-7792 Ref pgx metrics

--- metrics/pgx_metrics/README.md
+++ metrics/pgx_metrics/README.md
@@ -15,38 +15,44 @@
 ## Prometheus metrics example
 
 ```
-# HELP pgxpool_acquired_conns Current number of acquired connections in the pool.
-# TYPE pgxpool_acquired_conns gauge
-
-# HELP pgxpool_idle_conns Current number of idle connections in the pool.
-# TYPE pgxpool_idle_conns gauge
-
-# HELP pgxpool_total_conns Total number of connections in the pool.
-# TYPE pgxpool_total_conns gauge
-
-# HELP pgxpool_max_conns Maximum number of connections allowed in the pool.
-# TYPE pgxpool_max_conns gauge
-
-# HELP pgxpool_constructing_conns Number of connections currently being created.
-# TYPE pgxpool_constructing_conns gauge
-
-# HELP pgxpool_new_conns New connections being opened.
-# TYPE pgxpool_new_conns gauge
-
-# HELP pgxpool_acquire_count Total number of successful connection acquires.
+# HELP pgxpool_acquire_count Cumulative count of successful acquires from the pool.
 # TYPE pgxpool_acquire_count counter
 
-# HELP pgxpool_canceled_acquire_count Number of acquires canceled by context.
-# TYPE pgxpool_canceled_acquire_count counter
-
-# HELP pgxpool_empty_acquire_count Number of acquires that waited due to empty pool.
-# TYPE pgxpool_empty_acquire_count counter
-
-# HELP pgxpool_acquire_duration_seconds_total Total time spent acquiring connections.
+# HELP pgxpool_acquire_duration_seconds_total Total time spent acquiring connections (nanoseconds in source; should be converted to seconds).
 # TYPE pgxpool_acquire_duration_seconds_total counter
 
-# HELP pgxpool_empty_acquire_wait_seconds_total Total time waiting for connections when pool is empty.
+# HELP pgxpool_acquired_conns Number of currently acquired connections in the pool.
+# TYPE pgxpool_acquired_conns gauge
+
+# HELP pgxpool_canceled_acquire_count Cumulative count of acquires from the pool that were canceled by a context.
+# TYPE pgxpool_canceled_acquire_count counter
+
+# HELP pgxpool_constructing_conns Number of connections with construction in progress in the pool.
+# TYPE pgxpool_constructing_conns gauge
+
+# HELP pgxpool_empty_acquire_count Cumulative count of successful acquires that waited due to empty pool.
+# TYPE pgxpool_empty_acquire_count counter
+
+# HELP pgxpool_empty_acquire_wait_seconds_total Total time waiting for successful acquires due to empty pool (nanoseconds in source; should be converted to seconds).
 # TYPE pgxpool_empty_acquire_wait_seconds_total counter
+
+# HELP pgxpool_idle_conns Number of currently idle connections in the pool.
+# TYPE pgxpool_idle_conns gauge
+
+# HELP pgxpool_max_conns Maximum size of the pool.
+# TYPE pgxpool_max_conns gauge
+
+# HELP pgxpool_total_conns Total number of resources currently in the pool (constructing + acquired + idle).
+# TYPE pgxpool_total_conns gauge
+
+# HELP pgxpool_new_conns_count Cumulative count of new connections opened.
+# TYPE pgxpool_new_conns_count counter
+
+# HELP pgxpool_max_lifetime_destroy_count Cumulative count of connections destroyed due to MaxConnLifetime.
+# TYPE pgxpool_max_lifetime_destroy_count counter
+
+# HELP pgxpool_max_idle_destroy_count Cumulative count of connections destroyed due to MaxConnIdleTime.
+# TYPE pgxpool_max_idle_destroy_count counter
 ```
 
 ## Usage
--- metrics/pgx_metrics/collector.go
+++ metrics/pgx_metrics/collector.go
@@ -5,146 +5,118 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-// statsProvider abstracts pgxpool pool statistics access.
+// Stater abstracts pgxpool pool statistics access.
 // It exists to allow easier testing and decoupling from pgxpool implementation.
-type statsProvider interface {
+type Stater interface {
 	Stat() *pgxpool.Stat
 }
 
-// poolStatsCollector exports pgxpool connection pool statistics to Prometheus.
+type staterFunc func() *pgxpool.Stat
+
+// Collector exports pgxpool connection pool statistics to Prometheus.
 //
 // It implements prometheus.Collector and exposes both:
 //   - instantaneous pool state (gauges)
 //   - cumulative pool activity counters (from pgxpool.Stat)
-type poolStatsCollector struct {
-	statsProvider statsProvider
-	dbName        string
+type Collector struct {
+	stater staterFunc
 
-	acquiredConns     *prometheus.Desc
-	idleConns         *prometheus.Desc
-	totalConns        *prometheus.Desc
-	maxConns          *prometheus.Desc
-	constructingConns *prometheus.Desc
-	newConns          *prometheus.Desc
-
-	acquireCount      *prometheus.Desc
-	canceledAcquire   *prometheus.Desc
-	emptyAcquireCount *prometheus.Desc
-
-	acquireDuration  *prometheus.Desc
-	emptyAcquireWait *prometheus.Desc
+	acquireCount            *prometheus.Desc
+	acquireDuration         *prometheus.Desc
+	acquiredConns           *prometheus.Desc
+	canceledAcquireCount    *prometheus.Desc
+	constructingConns       *prometheus.Desc
+	emptyAcquireCount       *prometheus.Desc
+	emptyAcquireWaitTime    *prometheus.Desc
+	idleConns               *prometheus.Desc
+	maxConns                *prometheus.Desc
+	totalConns              *prometheus.Desc
+	newConnsCount           *prometheus.Desc
+	maxLifetimeDestroyCount *prometheus.Desc
+	maxIdleDestroyCount     *prometheus.Desc
 }
 
-// NewPoolStatsCollector creates a Prometheus collector for pgxpool statistics.
+// NewCollector creates a Prometheus collector for pgxpool statistics.
 //
 // Metrics are labeled with db_name and follow pgxpool naming conventions.
 // The collector reads a snapshot of pool state on each scrape.
-//
-//nolint:funlen
-func NewPoolStatsCollector(statsProvider statsProvider, dbName string) prometheus.Collector {
+func NewCollector(stater Stater, dbName string) prometheus.Collector {
+	fn := func() *pgxpool.Stat { return stater.Stat() }
+	return newCollector(fn, dbName)
+}
+
+// newCollector is an internal only constructor for a Collecter. It accepts
+// a stater which provides a closure for requesting pgxpool.Stat metrics.
+// Labels to each metric and may be nil. A label is recommended when an
+// application uses more than one pgxpool.Pool to enable differentiation between them.
+func newCollector(stater staterFunc, dbName string) *Collector {
 	fq := func(n string) string {
 		return "pgxpool_" + n
 	}
+	labels := prometheus.Labels{"db_name": dbName}
 
-	return &poolStatsCollector{
-		statsProvider: statsProvider,
-		dbName:        dbName,
-
-		acquiredConns: prometheus.NewDesc(
-			fq("acquired_conns"),
-			"Current number of acquired connections",
-			nil,
-			prometheus.Labels{"db_name": dbName},
-		),
-
-		idleConns: prometheus.NewDesc(
-			fq("idle_conns"),
-			"Current number of idle connections",
-			nil,
-			prometheus.Labels{"db_name": dbName},
-		),
-
-		totalConns: prometheus.NewDesc(
-			fq("total_conns"),
-			"Total number of connections in pool",
-			nil,
-			prometheus.Labels{"db_name": dbName},
-		),
-
-		maxConns: prometheus.NewDesc(
-			fq("max_conns"),
-			"Max pool size",
-			nil,
-			prometheus.Labels{"db_name": dbName},
-		),
-
-		constructingConns: prometheus.NewDesc(
-			fq("constructing_conns"),
-			"Connections being created",
-			nil,
-			prometheus.Labels{"db_name": dbName},
-		),
-
-		newConns: prometheus.NewDesc(
-			fq("new_conns"),
-			"New connections being opened",
-			nil,
-			prometheus.Labels{"db_name": dbName},
-		),
+	return &Collector{
+		stater: stater,
 
 		acquireCount: prometheus.NewDesc(
 			fq("acquire_count"),
-			"Total successful acquires",
-			nil,
-			prometheus.Labels{"db_name": dbName},
-		),
-
-		canceledAcquire: prometheus.NewDesc(
-			fq("canceled_acquire_count"),
-			"Canceled acquires",
-			nil,
-			prometheus.Labels{"db_name": dbName},
-		),
-
-		emptyAcquireCount: prometheus.NewDesc(
-			fq("empty_acquire_count"),
-			"Acquires that waited for connection",
-			nil,
-			prometheus.Labels{"db_name": dbName},
-		),
-
+			"Cumulative count of successful acquires from the pool.",
+			nil, labels),
 		acquireDuration: prometheus.NewDesc(
-			fq("acquire_duration_seconds_total"),
-			"Total acquire duration",
-			nil,
-			prometheus.Labels{"db_name": dbName},
-		),
-
-		emptyAcquireWait: prometheus.NewDesc(
-			fq("empty_acquire_wait_seconds_total"),
-			"Total wait time when pool was empty",
-			nil,
-			prometheus.Labels{"db_name": dbName},
-		),
+			fq("acquire_duration_ns"),
+			"Total duration of all successful acquires from the pool in nanoseconds.",
+			nil, labels),
+		acquiredConns: prometheus.NewDesc(
+			fq("acquired_conns"),
+			"Number of currently acquired connections in the pool.",
+			nil, labels),
+		canceledAcquireCount: prometheus.NewDesc(
+			fq("canceled_acquire_count"),
+			"Cumulative count of acquires from the pool that were canceled by a context.",
+			nil, labels),
+		constructingConns: prometheus.NewDesc(
+			fq("constructing_conns"),
+			"Number of conns with construction in progress in the pool.",
+			nil, labels),
+		emptyAcquireCount: prometheus.NewDesc(
+			fq("empty_acquire"),
+			"Cumulative count of successful acquires from the pool that waited for a resource to be released or constructed because the pool was empty.",
+			nil, labels),
+		emptyAcquireWaitTime: prometheus.NewDesc(
+			fq("empty_acquire_wait_time_ns"),
+			"Cumulative time in nanoseconds waited for successful acquires from the pool for a resource to be released or constructed because the pool was empty.",
+			nil, labels),
+		idleConns: prometheus.NewDesc(
+			fq("idle_conns"),
+			"Number of currently idle conns in the pool.",
+			nil, labels),
+		maxConns: prometheus.NewDesc(
+			fq("max_conns"),
+			"Maximum size of the pool.",
+			nil, labels),
+		totalConns: prometheus.NewDesc(
+			fq("total_conns"),
+			"Total number of resources currently in the pool. The value is the sum of ConstructingConns, AcquiredConns, and IdleConns.",
+			nil, labels),
+		newConnsCount: prometheus.NewDesc(
+			fq("new_conns_count"),
+			"Cumulative count of new connections opened.",
+			nil, labels),
+		maxLifetimeDestroyCount: prometheus.NewDesc(
+			fq("max_lifetime_destroy_count"),
+			"Cumulative count of connections destroyed because they exceeded MaxConnLifetime.",
+			nil, labels),
+		maxIdleDestroyCount: prometheus.NewDesc(
+			fq("max_idle_destroy_count"),
+			"Cumulative count of connections destroyed because they exceeded MaxConnIdleTime.",
+			nil, labels),
 	}
 }
 
 // Describe sends all metric descriptors to Prometheus.
 // It is required by prometheus.Collector.
-func (c *poolStatsCollector) Describe(ch chan<- *prometheus.Desc) {
-	ch <- c.acquiredConns
-	ch <- c.idleConns
-	ch <- c.totalConns
-	ch <- c.maxConns
-	ch <- c.constructingConns
-	ch <- c.newConns
-
-	ch <- c.acquireCount
-	ch <- c.canceledAcquire
-	ch <- c.emptyAcquireCount
-
-	ch <- c.acquireDuration
-	ch <- c.emptyAcquireWait
+func (c *Collector) Describe(ch chan<- *prometheus.Desc) {
+	prometheus.DescribeByCollect(c, ch)
 }
 
 // Collect gathers a snapshot of pgxpool statistics and exports them as Prometheus metrics.
@@ -154,19 +126,71 @@ func (c *poolStatsCollector) Describe(ch chan<- *prometheus.Desc) {
 //
 // Note: duration-related metrics are cumulative values provided by pgxpool.Stat
 // and are exported as counters for compatibility with Prometheus conventions.
-func (c *poolStatsCollector) Collect(ch chan<- prometheus.Metric) {
-	st := c.statsProvider.Stat()
-	ch <- prometheus.MustNewConstMetric(c.acquiredConns, prometheus.GaugeValue, float64(st.AcquiredConns()))
-	ch <- prometheus.MustNewConstMetric(c.idleConns, prometheus.GaugeValue, float64(st.IdleConns()))
-	ch <- prometheus.MustNewConstMetric(c.totalConns, prometheus.GaugeValue, float64(st.TotalConns()))
-	ch <- prometheus.MustNewConstMetric(c.maxConns, prometheus.GaugeValue, float64(st.MaxConns()))
-	ch <- prometheus.MustNewConstMetric(c.constructingConns, prometheus.GaugeValue, float64(st.ConstructingConns()))
-	ch <- prometheus.MustNewConstMetric(c.newConns, prometheus.GaugeValue, float64(st.NewConnsCount()))
-
-	ch <- prometheus.MustNewConstMetric(c.acquireCount, prometheus.CounterValue, float64(st.AcquireCount()))
-	ch <- prometheus.MustNewConstMetric(c.canceledAcquire, prometheus.CounterValue, float64(st.CanceledAcquireCount()))
-	ch <- prometheus.MustNewConstMetric(c.emptyAcquireCount, prometheus.CounterValue, float64(st.EmptyAcquireCount()))
-
-	ch <- prometheus.MustNewConstMetric(c.acquireDuration, prometheus.CounterValue, st.AcquireDuration().Seconds())
-	ch <- prometheus.MustNewConstMetric(c.emptyAcquireWait, prometheus.CounterValue, st.EmptyAcquireWaitTime().Seconds())
+func (c *Collector) Collect(metrics chan<- prometheus.Metric) {
+	st := c.stater()
+	metrics <- prometheus.MustNewConstMetric(
+		c.acquireCount,
+		prometheus.CounterValue,
+		float64(st.AcquireCount()),
+	)
+	metrics <- prometheus.MustNewConstMetric(
+		c.acquireDuration,
+		prometheus.CounterValue,
+		float64(st.AcquireDuration()),
+	)
+	metrics <- prometheus.MustNewConstMetric(
+		c.acquiredConns,
+		prometheus.GaugeValue,
+		float64(st.AcquiredConns()),
+	)
+	metrics <- prometheus.MustNewConstMetric(
+		c.canceledAcquireCount,
+		prometheus.CounterValue,
+		float64(st.CanceledAcquireCount()),
+	)
+	metrics <- prometheus.MustNewConstMetric(
+		c.constructingConns,
+		prometheus.GaugeValue,
+		float64(st.ConstructingConns()),
+	)
+	metrics <- prometheus.MustNewConstMetric(
+		c.emptyAcquireCount,
+		prometheus.CounterValue,
+		float64(st.EmptyAcquireCount()),
+	)
+	metrics <- prometheus.MustNewConstMetric(
+		c.emptyAcquireWaitTime,
+		prometheus.CounterValue,
+		float64(st.EmptyAcquireWaitTime()),
+	)
+	metrics <- prometheus.MustNewConstMetric(
+		c.idleConns,
+		prometheus.GaugeValue,
+		float64(st.IdleConns()),
+	)
+	metrics <- prometheus.MustNewConstMetric(
+		c.maxConns,
+		prometheus.GaugeValue,
+		float64(st.MaxConns()),
+	)
+	metrics <- prometheus.MustNewConstMetric(
+		c.totalConns,
+		prometheus.GaugeValue,
+		float64(st.TotalConns()),
+	)
+	metrics <- prometheus.MustNewConstMetric(
+		c.newConnsCount,
+		prometheus.CounterValue,
+		float64(st.NewConnsCount()),
+	)
+	metrics <- prometheus.MustNewConstMetric(
+		c.maxLifetimeDestroyCount,
+		prometheus.CounterValue,
+		float64(st.MaxLifetimeDestroyCount()),
+	)
+	metrics <- prometheus.MustNewConstMetric(
+		c.maxIdleDestroyCount,
+		prometheus.CounterValue,
+		float64(st.MaxIdleDestroyCount()),
+	)
 }
